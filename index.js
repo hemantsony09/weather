f499d937d20d91bf9882a5c40b3e87d2af2d55f8
hemant soni <105153291+hemantsony09@users.noreply.github.com>
2023-05-16 12:38:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,31 +48,31 @@ function showWeatherReport(weather){
 
     
     if(weatherType.textContent == 'Clear') {
-        document.body.style.backgroundImage = "url('images/clear.jpg')";
+        document.body.style.backgroundImage = "url('clear.jpg')";
         
     } else if(weatherType.textContent == 'Clouds') {
 
-        document.body.style.backgroundImage = "url('images/cloud.jpg')";
+        document.body.style.backgroundImage = "url('cloud.jpg')";
         
     } else if(weatherType.textContent == 'Haze') {
 
-        document.body.style.backgroundImage = "url('images/cloud.jpg')";
+        document.body.style.backgroundImage = "url('cloud.jpg')";
         
     }     else if(weatherType.textContent == 'Rain') {
         
-        document.body.style.backgroundImage = "url('images/rain.jpg')";
+        document.body.style.backgroundImage = "url('rain.jpg')";
         
     } else if(weatherType.textContent == 'Snow') {
         
-        document.body.style.backgroundImage = "url('images/snow.jpg')";
+        document.body.style.backgroundImage = "url('snow.jpg')";
     
     } else if(weatherType.textContent == 'Thunderstorm') {
     
-        document.body.style.backgroundImage = "url('images/thunderstorm.jpg')";
+        document.body.style.backgroundImage = "url('thunderstorm.jpg')";
         
     } else if(weatherType.textContent == 'Smoke') {
     
-        document.body.style.backgroundImage = "url('images/smoke.jpg')";
+        document.body.style.backgroundImage = "url('smoke.jpg')";
         
     } 
 }
